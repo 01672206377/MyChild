98c1feeacb4a69d166a7c52992a1f5125185c513
contain-code <tnmcong.20it7@vku.udn.vn>
2023-11-04 23:34:19 +0700

    push source code web

---
+++ webProject/src/public/js/slick-slide.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.my-slick-slide').slick({
+        slidesToShow: 4,
+        slidesToSroll:1,
+        focusOnSelect: true,
+        prevArrow: '<i class="fa-solid fa-chevron-left arrow-left"></i>',
+        nextArrow: '<i class="fa-solid fa-chevron-right arrow-right"></i>'
+    });
+});
